055f19b766e7029c8f9813b1c3e2dbb2fe98d949
Allison <allisonlyhuang@gmail.com>
2026-03-30 15:34:21 -0700

    feat: scanners page

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -4,6 +4,7 @@ import Footer from './components/Footer'
 import Home from './pages/Home'
 import Company from './pages/Company'
 import Printers from './pages/products/Printers'
+import Scanners from './pages/products/Scanners'
 import ProductOverview from './pages/products/ProductOverview'
 import Contact from './pages/Contact'
 import Support from './pages/Support'
@@ -17,6 +18,8 @@ function App() {
         <Route path="/company/*" element={<Company />} />
         <Route path="/printers/:model" element={<ProductOverview />} />
         <Route path="/printers/*" element={<Printers />} />
+        <Route path="/scanners/:model" element={<ProductOverview />} />
+        <Route path="/scanners/*" element={<Scanners />} />
         <Route path="/contact" element={<Contact />} />
         <Route path="/support" element={<Support />} />
       </Routes>
