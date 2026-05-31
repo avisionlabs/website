ae75f5abcb6eaf61b7f7387c5fd88cc732e5c288
Allison <allisonlyhuang@gmail.com>
2026-05-30 19:13:06 -0700

    chore: reinstall admin pw

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,7 +2,6 @@ import { lazy, Suspense } from 'react'
 import { Routes, Route, useLocation } from 'react-router-dom'
 import Navbar from './components/Navbar'
 import Footer from './components/Footer'
-import AdminPage from './pages/Admin/AdminPage'
 
 const Home = lazy(() => import('./pages/Home'))
 const Company = lazy(() => import('./pages/Company/About'))
@@ -11,7 +10,7 @@ const Scanners = lazy(() => import('./pages/Products/Scanners'))
 const ProductOverview = lazy(() => import('./pages/Products/ProductOverview'))
 const Contact = lazy(() => import('./pages/Contact/Contact'))
 const Support = lazy(() => import('./pages/Support/Support'))
-// const Admin = lazy(() => import('./pages/Admin/Admin'))
+const Admin = lazy(() => import('./pages/Admin/Admin'))
 
 function App() {
   const location = useLocation()
@@ -28,8 +27,7 @@ function App() {
             <Route path="/scanners/:model" element={<ProductOverview />} />
             <Route path="/scanners/*" element={<Scanners />} />
             <Route path="/contact" element={<Contact />} />
-            <Route path="/adminpage" element={<AdminPage />} />
-            {/* <Route path="/adminpage" element={<Admin />} /> */}
+            <Route path="/adminpage" element={<Admin />} />
             <Route path="/support" element={<Support />} />
           </Routes>
         </div>
